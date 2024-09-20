f9366872ad112c56b87ebb6363e2ea9c4b433c40
HP <HP@DESKTOP-NFIDR8T>
2024-09-19 18:56:19 -0700

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            String s = " Anil Metakari";
+            int i = s.Length - 1;
+            String reverse = "";
+            while(i>0)
+            {
+                reverse += s[i];
+                i--;
+                Console.WriteLine(reverse);
+
+            }
+
+
+            //string
+
+            string str = " Hello world";
+            str = "test";
+            Console.WriteLine(str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+       
+
+    }
+
+}
+
+
+
+
+
